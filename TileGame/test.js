1d46fd346cf5647d9211cc0d78f24d4972602af8
marybrantnall <marybrantnall@gmail.com>
2024-02-12 22:01:12 -0400

    adding selenium testing

---
+++ TileGame/test.js
@@ -0,0 +1,31 @@
+import { Builder, By, until } from 'selenium-webdriver'
+
+async function exampleTest() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  
+  try {
+
+    
+    await driver.get('http://localhost:5173');
+    
+
+    for (let i = 0; i < 6; i++) {
+
+      for (let j = 0; j < 6; j++) {
+
+        await driver.wait(until.elementLocated(By.id(`${i},${j}`), 10000))
+        await driver.findElement(By.id(`${i},${j}`)).click();
+          
+      }
+    }
+
+    await driver.executeScript('alert("Test sucessful")')
+
+  } finally {
+      console.log("data retrieved from API")
+      console.log("clickable tiles all able to be clicked.")
+  }
+  
+}
+
+exampleTest();
